0deaf4eb29f77d1b4dbdcf7825c6a0bb27c1f214
Marissa <yaridog@gmail.com>
2013-03-15 23:22:26 -0700

    update markov chain

--- markov.py
+++ markov.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+import random
 from sys import argv
 
 def make_chains(corpus):
@@ -10,32 +10,84 @@ def make_chains(corpus):
     words = input_text.split()
 
     counter = 0
-
-    while counter < len(words) - 2:
-
-        for index,word in enumerate(words): 
-            lyrics[words[counter], words[counter+1]] = words[counter+2]
-        counter += 1
-
-    print lyrics
+    #create chain
+    
+    for index,word in enumerate(words):
+        #counter must end before the end of the len to avoid out of range
+        while counter < len(words) - 3:
+            #check if word pair value exists
+            if (words[counter], words[counter+1]) not in lyrics:
+                lyrics[words[counter], words[counter+1]] = [words[counter+2]]
+            else:
+                #if true, add to list
+                #get the list from the dictionary and append the value
+                lyrics[words[counter], words[counter+1]].append(words[counter+2])
+            counter += 1
 
 
 def make_text(chains):
     """Takes a dictionary of markov chains and returns random text
     based off an original text."""
 
-    for key,value in lyrics.items():
-        #start the chain with key:value pair
-        starter = key,value
-        #key - tuple(0)
-        lyric_key = key[1]
-        #key - tuple(1)
-        lyric_value = value
-        #find the next value based on lyric_key,lyric_value tuple
-        nextword = lyrics[(lyric_key,lyric_value)]
-        print key[0],key[1], value, nextword
+
+    tupled_list = lyrics.items()
+    #get random dictionary key pair
+    dict_length = len(tupled_list)
+    starter_keypair = random.randint(0,dict_length)
+    starter_tuple = tupled_list[starter_keypair]
+    #print starter_tuple
+    i = 0
+    starter_key1 = starter_tuple[0][0]
+    #print starter_key1
+    starter_key2 = starter_tuple[0][1]
+    #print starter_key2
+
+    new_lyric = [starter_key1, starter_key2]
+
+    while i < 5:
         
-    return nextword
+        #key - tuple(0)
+        #lyric_key = key[1]
+            #key - tuple(1)
+
+            #get random value
+        
+        num_value = len(lyrics[starter_key1, starter_key2]) - 1
+        random_value = random.randint(0, num_value)
+        lyric_value = lyrics[starter_key1, starter_key2][random_value]
+            #find the next value based on lyric_key,lyric_value tuple
+            #nextword = lyrics[(lyric_key,lyric_value)][1]
+        #print starter_key1, starter_key2, lyric_value
+        new_lyric.append(lyric_value)
+        #new_starter = key[1], lyric_value
+        i += 1
+        starter_key1 = starter_key2
+        starter_key2 = lyric_value
+    new_lyric_txt =  " ".join(new_lyric)
+    print new_lyric_txt
+    
+    #for key,value in lyrics.items():
+        #start the chain with key:value pair
+        #print key,value
+        # while i < 5:
+        #     starter = key,value
+        #     #key - tuple(0)
+        #     lyric_key = key[1]
+        #     #key - tuple(1)
+
+        #     #get random value
+        #     num_value = len(lyrics[key[0], key[1]]) - 1
+        #     random_value = random.randint(0, num_value)
+        #     print num_value,random_value
+        #     lyric_value = lyrics[key[0], key[1]][random_value]
+        #     #find the next value based on lyric_key,lyric_value tuple
+        #     #nextword = lyrics[(lyric_key,lyric_value)][1]
+        #     print key[0],key[1], lyric_value
+        #     starter = key[1], lyric_value
+        #     i += 1
+
+        
+    #return nextword
 
 def main():
 
@@ -51,7 +103,7 @@ def main():
     f.close()
 
     chain_dict = make_chains(input_text)
-    #random_text = make_text(chain_dict)
+    random_text = make_text(chain_dict)
     # print random_text
 
 main()
